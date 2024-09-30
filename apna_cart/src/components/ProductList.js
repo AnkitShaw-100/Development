95162d5f76c57f3a14597e05339e64bd29b961e4
AnkitShaw_100 <ankitshaw6933@gmail.com>
2024-09-30 23:03:13 +0530

    commit

--- apna_cart/src/components/ProductList.js
+++ apna_cart/src/components/ProductList.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import { Component } from 'react'
-import React from 'react'
+import React from 'react';
 
 export default function ProductList(props) {
   return (
-    props.product.map((product) => {    )
-  )
+    <div>
+      {props.product.map((product, index) => (
+        <div key={index}>
+          <h2>{product.name}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
