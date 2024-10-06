e8efac2dce9e8cfa9db2c6d5125b2332db7c8ef5
AnkitShaw_100 <ankitshaw6933@gmail.com>
2024-10-06 20:25:26 +0530

    chai aur code react

---
+++ 01-vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur react | Ankit Shaw</h1>
+  )
+}
+
+export default App
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur react | Ankit Shaw</h1>
+  );
+}
+
+export default App;
