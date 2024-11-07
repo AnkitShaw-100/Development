04e08a082cd2cab1dd032c1b014ca00286ae4595
AnkitShaw_100 <ankitshaw6933@gmail.com>
2024-11-08 03:14:13 +0530

    hello world extension

---
+++ Project/Basic/23_hello_world_extension/popup.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('name').addEventListener('keyup', function () {
+//         const inputText = this.value || "world"; // Default to "world" if input is empty
+//         document.getElementById('greet').textContent = 'Hello ' + inputText;
+//     });
+// });
